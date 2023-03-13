df47a8783caad56f3caf8211f2b1d183cef29381
alex <alexey.rozhkov@clockwise.software>
2023-03-13 12:10:01 +0200

    add: all foreign keys cascade

--- migrations/20230308135223-mastersTownIdCascade.js
+++ migrations/20230308135223-mastersTownIdCascade.js
@@ -5,18 +5,142 @@ const { DataTypes, QueryInterface, Sequelize } = require("sequelize");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(QueryInterface, Sequelize) {
-    await await QueryInterface.removeConstraint(
-      "masters",
-      "masters_townIds_foreign_idx"
+    await QueryInterface.removeConstraint(
+      "reservations",
+      "reservations_master_id_foreign_idx"
     );
 
-    //onUpdate: "cascade",
-    //onDelete: "cascade",
+    await QueryInterface.removeConstraint(
+      "reservations",
+      "reservations_clientId_foreign_idx"
+    );
+
+    await QueryInterface.removeConstraint(
+      "reservations",
+      "reservations_towns_id_foreign_idx"
+    );
+
+    await QueryInterface.removeConstraint(
+      "masters",
+      "reservations_clientId_foreign_idx"
+    );
+
+    await QueryInterface.addConstraint("masters", {
+      fields: ["townId"],
+      type: "foreign key",
+      name: "masters_townId_foreign_idx",
+      references: {
+        table: "towns",
+        field: "id",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
+
+    await QueryInterface.addConstraint("reservations", {
+      fields: ["master_id"],
+      type: "foreign key",
+      name: "reservations_master_id_foreign_idx",
+      references: {
+        table: "masters",
+        field: "id",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
+
+    await QueryInterface.addConstraint("reservations", {
+      fields: ["towns_id"],
+      type: "foreign key",
+      name: "reservations_towns_id_foreign_idx",
+      references: {
+        table: "towns",
+        field: "id",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
+
+    await QueryInterface.addConstraint("reservations", {
+      fields: ["clientId"],
+      type: "foreign key",
+      name: "reservations_clientId_foreign_idx",
+      references: {
+        table: "clients",
+        field: "id",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
   },
 
   async down(QueryInterface, Sequelize) {
-    await QueryInterface.changeColumn("towns", "name", {
-      type: DataTypes.CHAR(255),
+    await QueryInterface.removeConstraint(
+      "reservations",
+      "reservations_master_id_foreign_idx"
+    );
+
+    await QueryInterface.removeConstraint(
+      "reservations",
+      "reservations_clientId_foreign_idx"
+    );
+
+    await QueryInterface.removeConstraint(
+      "reservations",
+      "reservations_towns_id_foreign_idx"
+    );
+
+    await QueryInterface.removeConstraint(
+      "masters",
+      "reservations_clientId_foreign_idx"
+    );
+
+    await QueryInterface.addConstraint("masters", {
+      fields: ["townId"],
+      type: "foreign key",
+      name: "masters_townId_foreign_idx",
+      references: {
+        table: "towns",
+        field: "id",
+      },
+      onUpdate: "restrict",
+      onDelete: "restrict",
+    });
+
+    await QueryInterface.addConstraint("reservations", {
+      fields: ["master_id"],
+      type: "foreign key",
+      name: "reservations_master_id_foreign_idx",
+      references: {
+        table: "masters",
+        field: "id",
+      },
+      onUpdate: "restrict",
+      onDelete: "restrict",
+    });
+
+    await QueryInterface.addConstraint("reservations", {
+      fields: ["towns_id"],
+      type: "foreign key",
+      name: "reservations_towns_id_foreign_idx",
+      references: {
+        table: "towns",
+        field: "id",
+      },
+      onUpdate: "restrict",
+      onDelete: "restrict",
+    });
+
+    await QueryInterface.addConstraint("reservations", {
+      fields: ["clientId"],
+      type: "foreign key",
+      name: "reservations_clientId_foreign_idx",
+      references: {
+        table: "clients",
+        field: "id",
+      },
+      onUpdate: "restrict",
+      onDelete: "restrict",
     });
   },
 };
