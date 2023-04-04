c35c49417e58767b13828a2c92db9fb001f95739
mip92 <oleksandr.boichenko@clockwise.software>
2023-04-04 12:03:21 +0300

    fix: mail problem

--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -1,19 +1,24 @@
 import * as nodemailer from "nodemailer";
 import { transporterCredentials } from "./constants";
 
+let transporter = nodemailer.createTransport({
+  ...transporterCredentials,
+});
+
 ////Отправка письма
 export async function sendMail(recipient, name, surname, rating) {
-  let transporter = nodemailer.createTransport({
-    ...transporterCredentials,
-  });
-
-  let result = await transporter.sendMail({
-    from: transporterCredentials.auth.user,
-    to: recipient,
-    subject: "Уведомление о резерве мастера",
-    text: "This message was sent from Node js server.",
-    html: `
-      Вы успешно заказали мастера ${name} ${surname} с рейтингом ${rating} 
-      `,
-  });
+  try {
+    let result = await transporter.sendMail({
+      from: transporterCredentials.auth.user,
+      to: recipient,
+      subject: "Уведомление о резерве мастера",
+      text: "This message was sent from Node js server.",
+      html: `
+        Вы успешно заказали мастера ${name} ${surname} с рейтингом ${rating} 
+        `,
+    });
+  } catch (e) {
+    console.log(e)
+  }
 }
+
